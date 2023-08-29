f994aec15546d71f4f99c2c4c5567cbf233b5346
MeriemBoujamza <kazahayakun@gmail.com>
2023-08-29 17:58:50 +0200

    first half done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { WeatherApiService } from './services/weather-api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skycast';
+
+  constructor(private weatherApiService: WeatherApiService ) {}
+
+  ngOnInit(): void {
+    this.weatherApiService.getWeatherData("new york")
+    .subscribe({
+      next: (resp) => {
+        console.log(resp)
+      }
+    })
+  }
 }
---
+++ src/app/services/weather-api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { WeatherData } from '../modules/weather.module';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherApiService {
+
+  constructor(private http: HttpClient) {
+
+   }
+
+
+  getWeatherData(cityName : string) : Observable<WeatherData> {
+
+    return this.http.get<WeatherData>('https://weatherapi-com.p.rapidapi.com/current.json', {
+      headers: new HttpHeaders()
+        .set(
+          'X-RapidAPI-Key',
+          'b585571fc8msha2d6558a951a226p1d7e54jsn60f3e0940558'
+        )
+        .set('X-RapidAPI-Host', 'open-weather13.p.rapidapi.com'),
+
+        params: new HttpParams().set("q",cityName)
+    });
+
+  }
+}
